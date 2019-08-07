49660e4d7ecba9e836298373deb98bf5c178990c
seineo <50976339+seineo@users.noreply.github.com>
2019-08-07 17:40:52 +0800

    Create 3.2.2_list.h

---
+++ ch03/3.2.2_list.h
@@ -0,0 +1,29 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+#include<cstddef>
+
+class Node;
+using PtrToNode = class Node*;
+using  List = PtrToNode;
+using  Position = PtrToNode;
+
+class Node {
+public:
+	List MakeEmpty();
+	bool IsEmpty() const { return next == NULL; }
+	bool IsLast(Position p) const { return p->next == NULL; }
+	Position Find(int);
+	void Delete(int);
+	Position FindPrevious(int);
+	void Insert(int, Position);
+	void DeleteList();
+	Position Header() { return this; }
+	Position First() const { return next; };
+	int Retrieve(Position p) const { return p->element; };
+private:
+	int element;
+	Position next;
+};
+
+#endif // !_LIST_H

f4e4f4d0c15bf74f270eaaa3c00195d087d6a5bf
seineo <50976339+seineo@users.noreply.github.com>
2019-08-09 17:22:10 +0800

    Create list_test.cpp

---
+++ ch03/list_test.cpp
@@ -0,0 +1,35 @@
+#include"List.h"
+using namespace std;
+
+int main()
+{
+	LinkedList list;  
+	cin >> list;
+	cout << list << endl;
+	
+	LinkedList list2(list);  
+	list2.Delete(5);
+	cout << list2 << endl;
+
+	LinkedList list3 = list2;
+	list3.DeleteList();
+	cout << list3 << endl;
+	cout << boolalpha << list3.IsEmpty() << endl;
+	cout << list3.First() << endl;
+
+	return 0;
+}
+
+//input:
+//2 3 4 5 6 7
+//^Z
+
+//output"
+//2 3 4 5 6 7
+//count = 6
+//2 3 4 6 7
+//count = 5
+//
+//count = 0
+//true
+//00000000   (nullptr)

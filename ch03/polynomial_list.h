18c080ecc5a32d33b845f1cc234140ffeb10b1c5
seineo <50976339+seineo@users.noreply.github.com>
2019-08-10 23:13:19 +0800

    Create polynomial_list.h

---
+++ ch03/polynomial_list.h
@@ -0,0 +1,36 @@
+#ifndef _POLYNOMIAL_LIST_H
+#define _POLYNOMIAL_LSIT_H
+
+#include<iostream>
+
+class Node {
+public:
+	Node() :coeff(0), expo(0), next(nullptr) {}
+	Node(int co, int ex) :coeff(co), expo(ex), next(nullptr) {}
+	Node(const Node&) = delete;
+	Node& operator=(const Node&) = delete;
+
+	int coeff;
+	int expo;
+	Node* next;
+};
+
+class Polynomial {
+	friend std::istream& operator>>(std::istream&, Polynomial&);
+	friend std::ostream& operator<<(std::ostream&, const Polynomial&);
+	friend Polynomial operator+(const Polynomial&, const Polynomial&);
+	friend Polynomial operator*(const Polynomial&, const Polynomial&);
+public:
+	Polynomial() = default;
+	Polynomial(const Polynomial&);
+	Polynomial& operator=(Polynomial);
+	~Polynomial();
+
+	Polynomial& operator+=(const Polynomial&);
+	Polynomial& operator*=(const Polynomial&);
+
+private:
+	Node* head = new Node; //note that it's a dummy node(also called header)!!!
+};
+
+#endif // !_POLYNOMIAL_LIST_H

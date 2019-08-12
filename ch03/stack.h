2cd59be17f0d0e8aea8f08a63e908fc925c3815d
seineo <50976339+seineo@users.noreply.github.com>
2019-08-12 22:22:13 +0800

    Create stack.h

---
+++ ch03/stack.h
@@ -0,0 +1,37 @@
+#ifndef _STACK_H
+#define _STACK_H
+
+#include<iostream>
+
+class Node {
+public:
+	Node() :data(0), next(nullptr) {}
+	Node(int item) :data(item), next(nullptr) {}
+	Node(const Node&) = delete;
+	Node& operator=(const Node&) = delete;
+
+	int data;
+	Node* next;
+};
+
+class Stack {
+	friend std::istream& operator>>(std::istream&, Stack&);      // I/O is just for test
+	friend std::ostream& operator<<(std::ostream&, Stack);
+public:
+	Stack() = default;
+	Stack(const Stack&);
+	Stack& operator=(Stack);
+	~Stack();
+
+	bool IsEmpty() const { return head->next == nullptr; }
+	void MakeEmpty();
+	void Push(int);
+	int Top();
+	void Pop();
+
+private:
+	Node* head = new Node;   //dummy node
+};
+
+
+#endif // !_STACK_H

df925579ebbc1c3bb454725149e5d0249cfd635c
seineo <50976339+seineo@users.noreply.github.com>
2019-08-13 18:28:58 +0800

    Create stack_array.h

---
+++ ch03/stack_array.h
@@ -0,0 +1,35 @@
+#ifndef _STACK_ARRAY_H
+#define _STACK_ARRAY_H
+
+constexpr int EmptyTOS = -1;
+const int MinStackSize = 5;
+
+struct StackRecord {
+	StackRecord() = default;
+	StackRecord(int cap, int tos) :capacity(cap), top_of_stack(tos) { array = new int[capacity]; }
+	
+	int capacity;
+	int top_of_stack;
+	int* array;
+};
+
+class Stack {
+public:           //without I/O this time
+	Stack(int);
+	Stack(const Stack&);
+	Stack& operator=(Stack);
+	~Stack();
+
+	bool IsEmpty() const { return stack->top_of_stack == EmptyTOS; }
+	bool IsFull() const { return stack->top_of_stack == stack->capacity; }
+	void MakeEmpty();
+	void Push(int);
+	int Top() const;
+	void Pop();
+	int TopAndPop();
+
+private:
+	StackRecord* stack;
+};
+
+#endif // !_STACK_ARRAY_H

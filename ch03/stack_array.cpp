1ac6b684e7706c1749dcb1e0045e016fbf3c2097
seineo <50976339+seineo@users.noreply.github.com>
2019-09-04 14:46:43 +0800

    Update stack_array.cpp

--- ch03/stack_array.cpp
+++ ch03/stack_array.cpp
@@ -6,7 +6,7 @@ Stack::Stack(int max_elements)
 {
 	if (max_elements < MinStackSize)
 		throw std::runtime_error("Stack size is too small");
-	stack = new StackRecord(max_element);
+	stack = new StackRecord(max_elements, EmptyTOS);
 }
 
 Stack::Stack(const Stack &s)

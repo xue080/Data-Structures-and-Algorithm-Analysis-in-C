ddd8d2d4b60e7dd94e0129c61b7ffb8d806272c5
seineo <1013248644qq@gmail.com>
2020-01-14 21:48:55 +0800

    reformat code

--- ch06/selection_problem-6B.cpp
+++ ch06/selection_problem-6B.cpp
@@ -11,15 +11,15 @@ using namespace std;
 
 vector<int> heap;
 
-void PercolateDown(int i,int k) {
+void PercolateDown(int i, int k) {
     int child;
     int temp = heap[i];
-    for(;i * 2 <= k;i = child) {
+    for (; i * 2 <= k; i = child) {
         child = i * 2;
-        if(child != k && heap[child + 1] < heap[child]) {
+        if (child != k && heap[child + 1] < heap[child]) {
             child++;
         }
-        if(temp > heap[child]) {
+        if (temp > heap[child]) {
             heap[i] = heap[child];
         } else {
             break;
@@ -28,38 +28,38 @@ void PercolateDown(int i,int k) {
     heap[i] = temp;
 }
 
-void Insert(int x,int k) {
+void Insert(int x, int k) {
     int i;
     heap[1] = x;   //equal to delete min and insert x
-    PercolateDown(1,k);
+    PercolateDown(1, k);
 }
 
 void BuildHeap(int k) {
-    for(int i = k / 2;i > 0;--i) {
-        PercolateDown(i,k);
+    for (int i = k / 2; i > 0; --i) {
+        PercolateDown(i, k);
     }
 }
 
 int Select(int N, int k) {
     int data;
     heap.resize(k + 1);
-    for(int i = 1;i <= k;++i) {
+    for (int i = 1; i <= k; ++i) {
         cin >> heap[i];
     }
     heap[0] = MinData;
     BuildHeap(k);  //min heap
-    for(int i = 0;i != N - k;++i) {
+    for (int i = 0; i != N - k; ++i) {
         cin >> data;
-        if(data > heap[1]) {
-            Insert(data,k);
+        if (data > heap[1]) {
+            Insert(data, k);
         }
     }
     return heap[1];
 }
 
 int main() {
-    int N,k;
+    int N, k;
     cin >> N >> k;
-    cout << Select(N,k) << endl;
+    cout << Select(N, k) << endl;
     return 0;
 }
